3c465c4549c120e9011b9b5d81c3eb7ba6260447
Afakaso <99641651+Afakaso@users.noreply.github.com>
2022-02-14 15:46:04 +0300

    Delete feedback.php

--- feedback_form/feedback.php
+++
@@ -1,7 +0,0 @@
-<?php
-$email = $_POST["email"];
-$send = mail ($email,$header,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
-if ($send == 'true')
-    {echo "Message sent";}
-else {echo "Something went wrong";}
-?>
